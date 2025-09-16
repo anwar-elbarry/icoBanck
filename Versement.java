bdda3e5780d4892e81a303a4d045d896f26957c5
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-16 15:17:59 +0100

    create versement class

--- Versement.java
+++ Versement.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Versement {
+public class Versement extends operation{
+   protected ArrayList<String> source = new ArrayList<>(Arrays.asList( "Virement externe", "Dépôt espèces", "Salaire"));
+    public Versement(double montant) {
+        super(montant);
+    }
 }
