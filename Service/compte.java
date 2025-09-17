a81db0ae1f1e6c677997aa3a3275426873952e15
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-17 14:46:31 +0100

    Ãcreate constructor and setup setters

--- Service/compte.java
+++ Service/compte.java
@@ -1,11 +1,27 @@
+package Service;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class compte {
+
  protected String code ;
  protected double solde ;
- protected ArrayList<String> listeOperations;
+ private List<operation> listOperations = new ArrayList<>();
 
- public abstract void retirer(double montant);
+ public compte(String code,double montant){
+   this.solde = montant;
+   this.code = "CPT-"+code;
+ }
+
+ public String getCode(){return code;};
+ public double getSolde(){return solde;};
+
+ public abstract void retirer(String code,double montant);
  public abstract double calculerInteret();
  public abstract void afficherDetails();
+
+ public void ajouterOperation(operation op){
+  listOperations.add(op);
+ }
 }
