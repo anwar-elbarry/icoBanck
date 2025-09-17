96280c7a04b146fdc61127bc1d9b102584b0b81c
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-17 14:47:53 +0100

    complete principale functions

--- Service/compteCourant.java
+++ Service/compteCourant.java
@@ -1,16 +1,30 @@
-public class CompteCourant extends compte{
+package Service;
+
+public class compteCourant extends compte {
 
  protected double decouvert;
+
+     public compteCourant(String code , double montant,double decouvert){
+         super(code,montant);
+         this.decouvert = decouvert;
+     }
+
     @Override
-    public void retirer(double montant) {
-        return;
+    public void retirer(String code,double montant) {
+         if(solde - montant >= -decouvert){
+             this.solde -= montant;
+         }else{
+             System.out.println("⚠\uFE0F Retrait refusé : dépassement du découvert autorisé.");
+         }
     }
     @Override
     public  double calculerInteret(){
         return 0;
     }
+
     @Override
     public void afficherDetails(){
-        return;
+        System.out.println("code : "+code);
+        System.out.println("solde : "+solde+"DH");
     }
 }
