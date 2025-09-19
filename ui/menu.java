24d1593baedb46c82a2bd491f4846b6fbfdeffa7
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-19 11:04:40 +0100

    complete ui menu

--- ui/menu.java
+++ ui/menu.java
@@ -1,37 +1,40 @@
-import java.io.InputStream;
+package ui;
+import Service.BankService;
 import java.util.Scanner;
 
 public class menu {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
+    public static BankService bank = new BankService();
+    public static void main(String[] args) throws Exception {
+
         boolean quitter = false;
 
         while(!quitter){
-            System.out.println("\n===== MENU PRINCIPAL =====");
-            System.out.println("1. Créer un compte");
-            System.out.println("2. Effectuer un versement");
-            System.out.println("3. Effectuer un retrait");
-            System.out.println("4. Effectuer un virement");
-            System.out.println("5. Consulter solde");
-            System.out.println("6. Consulter opérations");
-            System.out.println("0. Quitter");
-            System.out.print("Votre choix : ");
-
+            uiMenu();
             int choix = sc.nextInt();
-
             switch (choix) {
                 case 1:
-                    System.out.println("👉 Création d'un compte (exemple)");
+                    ajouterCompte();
                     break;
                 case 2:
-                    System.out.println("👉 Versement effectué (exemple)");
+                    effectuerVersement();
                     break;
-                case 3:
-                    System.out.println("👉 Retrait effectué (exemple)");
+                    case 3:
+                        effectuerRetrait();
                     break;
                 case 4:
-                    System.out.println("👉 Votre solde est : ...");
+                    effectuearVirment();
                     break;
+                case 5:
+                    getSolde();
+                    break;
+                case 6 :
+                    getOperations();
+                    break;
+                case 7 :
+                bank.showList();
+                break;
+
                 case 0:
                     quitter = true;
                     System.out.println("✅ Programme terminé.");
@@ -40,6 +43,119 @@ public class menu {
                     System.out.println("❌ Choix invalide !");
             }
         }
+
        sc.close();
+    }
+
+    public static void uiMenu (){
+        System.out.println("\n===== MENU PRINCIPAL =====");
+        System.out.println("1. Créer un compte");
+        System.out.println("2. Effectuer un versement");
+        System.out.println("3. Effectuer un retrait");
+        System.out.println("4. Effectuer un virement");
+        System.out.println("5. Consulter solde");
+        System.out.println("6. Consulter opérations");
+        System.out.println("0. Quitter");
+        System.out.print("Votre choix : ");
+    }
+
+    public static void ajouterCompte(){
+        System.out.println("choiser le type de votre compte :(1)Courant / (2)Epargne");
+        int type = sc.nextInt();
+        if (type == 1){
+            System.out.println("deposer un montant : ");
+            int montant = sc.nextInt();
+            System.out.println("ecreer un code pour votre compte(5chiffre) : CPT-");
+            String code = sc.next();
+            if (!code.matches("\\d{5}")){
+                System.out.println("Code invalide. Entrez exactement 5 chiffres (ex : 01234).");
+            }else{
+                System.out.println("ecreer votre decouvert : ");
+                int decouvert = sc.nextInt();
+                bank.creerCompteCourant(code,montant,decouvert);
+                System.out.println("Votre compte a ete creer avec success");
+            }
+        } else if (type == 2) {
+            System.out.println("deposer un montant : ");
+            int montant = sc.nextInt();
+            System.out.println("ecreer un code pour votre compte : CPT-");
+            String code = sc.next();
+            if (!code.matches("\\d{5}")) {
+                System.out.println("Code invalide. Entrez exactement 5 chiffres (ex : 01234).");
+            }else {
+                System.out.println("ecreer votre tauxInteret : ");
+                int tauxInteret = sc.nextInt();
+                bank.creerCompteEpargne(code,montant,tauxInteret);
+                System.out.println("Votre compte a ete creer avec success");
+            }
+
+        }else {
+            System.out.println("❌ Choix invalide !");
+            uiMenu();
+        }
+    }
+
+    public static void effectuerVersement(){
+        System.out.println("veuiz entrez le code de votre compte (CPT-XXXXX):");
+        String code = sc.next();
+        boolean isCompte = bank.trouverCompte(code);
+        if (isCompte){
+            System.out.println("veuiez entrez le montant :");
+            int montant = sc.nextInt();
+            System.out.println("veuiez entrez le source :");
+            String source = sc.next();
+            bank.effectuerVersement(code,montant,source);
+        }else {
+            System.out.println("Aucun compte trouvé avec ce code.");
+        }
+    }
+
+    public static void getSolde(){
+        System.out.println("veuiz entrez le code de votre compte (CPT-XXXXX):");
+        String code = sc.next();
+        boolean isCompte = bank.trouverCompte(code);
+        if(isCompte){
+            System.out.println("👉 Votre solde est : "+bank.trouverSolde(code)+" DH");
+        }else{
+            System.out.println("Aucun compte trouvé avec ce code.");
+        }
+    }
+
+    public static void getOperations(){
+        System.out.println("veuiz entrez le code de votre compte (CPT-XXXXX):");
+        String code = sc.next();
+        boolean isCompte = bank.trouverCompte(code);
+        if(isCompte){
+               bank.ListOperations(code);
+        }else{
+            System.out.println("Aucun compte trouvé avec ce code.");
+        }
+    }
+
+    public static void effectuerRetrait(){
+        System.out.println("veuiz entrez le code de votre compte (CPT-XXXXX):");
+        String code = sc.next();
+        boolean isCompte = bank.trouverCompte(code);
+        if (isCompte){
+            System.out.println("veuiez entrez le montant :");
+            int montant = sc.nextInt();
+            System.out.println("veuiez entrez la destination :");
+            String destination = sc.next();
+            bank.effectuerRetrait(code,montant,destination);
+            System.out.println("votre retraita ete effectuer avec success");
+        }else {
+            System.out.println("Aucun compte trouvé avec ce code.");
+        }
+    }
+
+    public static void effectuearVirment(){
+        System.out.println("veuiz entrez le code de votre compte (CPT-XXXXX):");
+        String src = sc.next();
+        System.out.println("veuiz entrez le code de compte de destination  (CPT-XXXXX):");
+        String dis = sc.next();
+            System.out.println("veuiez entrez le montant :");
+            int montant = sc.nextInt();
+            bank.effectuerVirment(src,dis,montant);
+            System.out.println("votre virment a été effectuer avec success");
     }
 }
